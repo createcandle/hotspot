46ea40a6592f3e00eb8e4a438b4360baeec5b408
flatsiedatsie <unfold@gmail.com>
2025-12-27 16:32:39 +0100

    Update hotspot_device.py

--- pkg/hotspot_device.py
+++ pkg/hotspot_device.py
@@ -191,7 +191,7 @@ class HotspotProperty(Property):
 
     def update(self, value):         
         if self.device.adapter.DEBUG:
-            print("property -> update. Value = " + str(value))
+            print("hotspot debug: property -> update. Value = " + str(value))
         
         if value != self.value:
             
